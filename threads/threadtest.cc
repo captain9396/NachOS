ceb9be78764b69ad94a142c7fbd88a598991d5d5
=captain9396 <=mostofamonsur9396@gmail.com>
2018-01-20 03:40:46 +0600

    commit 2

--- threads/threadtest.cc
+++ threads/threadtest.cc
@@ -14,8 +14,6 @@
 #include "synch.h"
 #include "unistd.h"
 #include "stdlib.h"
-#include "consumer.h"
-#include "producer.h"
 #include "bits/stdc++.h"
 #define SLEEPTIME 0
 #define DELAY 1000000000
@@ -48,75 +46,6 @@ Lock* lock = new Lock("myLock");
 
 
 
-string to_string(int x){
-	string val = "";
-	do{ val = ((char)((x%10) + '0')) + val; x/=10; }while(x);
-	return val;
-}
-
-
-
-void simpleProduce(void* arg){
-	while(true){
-		Producer producer = *((Producer*)arg);
-		if(items.size() == MAX){
-			cout << producer.getName()<< " cannot add to buffer : buffer FULL" << endl;
-//			sleep(SLEEPTIME);
-			for(int i = 0 ; i < DELAY; i++); // delay
-			producer_condition -> Wait(lock);
-		}
-
-
-		lock->Acquire();
-		producer.produce();
-
-		for(int i = 0 ; i < DELAY; i++); // delay
-
-		lock->Release();
-
-		if(items.size() == 1){
-			consumer_condition->Broadcast(lock);
-		}
-		currentThread -> Yield();
-	}
-}
-
-
-
-void simpleConsume(void* arg){
-	while(true){
-		Consumer consumer = *((Consumer*)arg);
-
-		if(items.size() == 0){
-			cout << consumer.getName()<< " cannot take from buffer : buffer EMPTY" << endl;
-			//sleep(SLEEPTIME);
-			for(int i = 0 ; i < DELAY; i++); // delay
-			consumer_condition -> Wait(lock);
-		}
-
-
-		lock->Acquire();
-		consumer.consume();
-		//sleep(SLEEPTIME);
-		for(int i = 0 ; i < DELAY; i++); // delay
-		lock->Release();
-
-		if (items.size() == MAX - 1){
-            producer_condition -> Broadcast(lock);
-        }
-		currentThread -> Yield();
-	}
-}
-
-
-void do_tas(void* arg){
-	
-	printf("CURRENT THREAD = %s\n", currentThread -> getName());
-	return ;
-}
-
-
-
 
 
 
@@ -132,48 +61,5 @@ void do_tas(void* arg){
 
 void ThreadTest(){
     DEBUG('t', "Entering SimpleTest");
-
-	int no_of_producers;
-	int no_of_consumers;
-
-
-    cout << "Enter # of Producers: " ;
-    cin >> no_of_producers;
-    cout << "Enter # of Consumers: " ;
-    cin >> no_of_consumers;
-
-	cout << "Maximum size of BUFFER: ";
-	cin >> MAX;
-
-	cout << "\n\n --------------------------------------------------------" << endl << endl;
-
-
-
-
-   	for(int i = 0 ; i < no_of_producers; i++){
-   		string name = "Producer " + to_string(i+1);
-   		Thread* t1 = new Thread("test");
-   		Producer* producer = new Producer(strdup(name.c_str()));
-   		t1->Fork((VoidFunctionPtr) simpleProduce, producer);
-
-   	}
-
-
-	for(int i = 0 ; i < no_of_consumers; i++){
-		string name = "Consumer " + to_string(i+1);
-		Thread* t2 = new Thread("test");
-   		Consumer* consumer = new Consumer(strdup(name.c_str()));
-
-   		t2->Fork((VoidFunctionPtr) simpleConsume, consumer);
-   	}
-   	
-   	
-   	
-   	/*Thread* t = new Thread("first");
-   	Thread* t2 = new Thread("second");
-   	printf("current thread = %s\n", currentThread -> getName());
-   	t2->Fork(do_tas, NULL);
-	t->Fork(do_tas, NULL);*/
-
 
 }
--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -1,19 +1,4 @@
-// addrspace.cc
-//	Routines to manage address spaces (executing user programs).
-//
-//	In order to run a user program, you must:
-//
-//	1. link with the -N -T 0 option
-//	2. run coff2noff to convert the object file to Nachos format
-//		(Nachos object code format is essentially just a simpler
-//		version of the UNIX executable object code format)
-//	3. load the NOFF file into the Nachos file system
-//		(if you haven't implemented the file system yet, you
-//		don't need to do this last step)
-//
-// Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation
-// of liability and disclaimer of warranty provisions.
+
 
 #include "copyright.h"
 #include "system.h"
@@ -29,13 +14,6 @@ int minimum(int a, int b){
 	if(a < b) return a;
 	else return b;
 }
-
-//----------------------------------------------------------------------
-// SwapHeader
-// 	Do little endian to big endian conversion on the bytes in the
-//	object file header, in case the file was generated on a little
-//	endian machine, and we're now running on a big endian machine.
-//----------------------------------------------------------------------
 
 static void
 SwapHeader (NoffHeader *noffH)
@@ -58,23 +36,6 @@ SwapHeader (NoffHeader *noffH)
 bool check_valid_code_and_data(unsigned int a, unsigned int b){
 	return (a > 0 || b > 0);
 }
-
-
-
-//----------------------------------------------------------------------
-// AddrSpace::AddrSpace
-// 	Create an address space to run a user program.
-//	Load the program from a file "executable", and set everything
-//	up so that we can start executing user instructions.
-//
-//	Assumes that the object code file is in NOFF format.
-//
-//	First, set up the translation from program memory to physical
-//	memory.  For now, this is really simple (1:1), since we are
-//	only uniprogramming, and we have a single unsegmented page table
-//
-//	"executable" is the file containing the object code to load into memory
-//----------------------------------------------------------------------
 
 AddrSpace::AddrSpace(OpenFile *executable, int space_id)
 {
@@ -109,18 +70,7 @@ AddrSpace::AddrSpace(OpenFile *executable, int space_id)
 	Size = size;
     numPages = divRoundUp(size, PageSize);
     size = numPages * PageSize;
-    
-    
 
-
-
-	//    ASSERT(numPages <= NumPhysPages);		// check we're not trying
-						// to run anything too big --
-						// at least until we have
-						// virtual memory
-	/*if(memory_manager -> GetUnallocatedPages() < numPages){
-		printf("Memory Insufficient! Cannot Allocate Page.\n");
-	}*/
 
     DEBUG('a', "Initializing address space, num pages %d, size %d\n",
 				numPages, size);
@@ -138,102 +88,12 @@ AddrSpace::AddrSpace(OpenFile *executable, int space_id)
 		pageTable[i].use = false;
 		pageTable[i].dirty = false;
 		pageTable[i].readOnly = false;  // if the code segment was entirely on
-						// a separate page, we could set its
-						// pages to be read-only
+				
     }
 
 
-
-
-
-
-
-// looping through each of the virtual
-// pages and zeroing that page
-// get a pointer to the physical page
-// by mapping the virtual page to the
-// physical page and zero the physical page
-// using the pointer to it
-
-//	let's say you want to
-// reference page 3 (the fourth page since page numbers start at 0).
-// Page 3 has a "physical address" of "3 * PageSize", and therefore
-// begins at machine->mainMemory[3 * PageSize]. To get a pointer to Page
-// 3, you would take its address: &machine->mainMemory[3 * PageSize].
-/*
-    for( i = 0; i < numPages; i++){
-		bzero(&(machine -> mainMemory[pageTable[i].physicalPage * PageSize]), PageSize);
-	}
-
-
-
-
-
-	//
-	total_page = divRoundUp((noffH.initData.size + noffH.code.size), PageSize);
-
-
-*/
-
-
-
-/*	GIVEN PSEUDO-CODE
-	----------------------
-	while (still code (or data) in executable file) {
-		file_off = determine current offset into executable file;
-		virt_addr = determine the virtual address that the code gets loaded at;
-		phys_addr = convert the virt_addr to a physical address (a pointer
-		into machine->mainMemory) using your page table;
-		size = PageSize;
-		executable->ReadAt(phys_addr, size, file_off)
-	}
-
-	here offset = noffH.code.inFileAddr*/
-
-
-/*
-	// checking if the program being loaded has valid code and data sections
-	if(check_valid_code_and_data(noffH.initData.size,noffH.code.size)){
-
-	// looping through all pages to read contents of file 'executable' into main memory page by page
-        for (int i = 0; i < total_page; i++){
-	        int file_offset = noffH.code.inFileAddr + (i * PageSize);
-        	char* physical_address = &(machine -> mainMemory[pageTable[i].physicalPage * PageSize]);
-
-    	    executable -> ReadAt( physical_address, PageSize, file_offset);
-        }
-	}
-	*/
-
-
-/*
-	// PREVIOUS SCHEME WHICH CANNOT BE USED
-	// AS WE ARE CREATING MULTIPROGRAMMING
-	// SUPPORT
-
-
-	then, copy in the code and data segments into memory
-    if (noffH.code.size > 0) {
-        DEBUG('a', "Initializing code segment, at 0x%x, size %d\n",
-			noffH.code.virtualAddr, noffH.code.size);
-        executable->ReadAt(&(machine->mainMemory[noffH.code.virtualAddr]),
-			noffH.code.size, noffH.code.inFileAddr);
-    }
-    if (noffH.initData.size > 0) {
-        DEBUG('a', "Initializing data segment, at 0x%x, size %d\n",
-			noffH.initData.virtualAddr, noffH.initData.size);
-        executable->ReadAt(&(machine->mainMemory[noffH.initData.virtualAddr]),
-			noffH.initData.size, noffH.initData.inFileAddr);
-    }
-*/
 
 }
-
-
-//----------------------------------------------------------------------
-// AddrSpace::~AddrSpace
-// 	Dealloate an address space.  Nothing for now!
-//----------------------------------------------------------------------
 
 AddrSpace::~AddrSpace()
 {
@@ -259,39 +119,18 @@ AddrSpace::InitRegisters()
 
     for (i = 0; i < NumTotalRegs; i++)
 	machine->WriteRegister(i, 0);
-
-    // Initial program counter -- must be location of "Start"
     machine->WriteRegister(PCReg, 0);
 
-    // Need to also tell MIPS where next instruction is, because
-    // of branch delay possibility
     machine->WriteRegister(NextPCReg, 4);
 
-   // Set the stack register to the end of the address space, where we
-   // allocated the stack; but subtract off a bit, to make sure we don't
-   // accidentally reference off the end!
     machine->WriteRegister(StackReg, numPages * PageSize - 16);
     DEBUG('a', "Initializing stack register to %d\n", numPages * PageSize - 16);
 }
 
-//----------------------------------------------------------------------
-// AddrSpace::SaveState
-// 	On a context switch, save any machine state, specific
-//	to this address space, that needs saving.
-//
-//	For now, nothing!
-//----------------------------------------------------------------------
 
 void AddrSpace::SaveState()
 {}
 
-//----------------------------------------------------------------------
-// AddrSpace::RestoreState
-// 	On a context switch, restore the machine state so that
-//	this address space can run.
-//
-//      For now, tell the machine where to find the page table.
-//----------------------------------------------------------------------
 
 void AddrSpace::RestoreState()
 {
